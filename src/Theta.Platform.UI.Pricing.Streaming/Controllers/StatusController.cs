c23a517cbefc9042f1ad192513369c5a3f00094e
Simon Ness <simon.ness@ringleadersolutions.com>
2018-12-10 13:06:10 +0100

    PD-41: Set service assembly version number at CI time.  Expose version with a status controller for now.

---
+++ src/Theta.Platform.UI.Pricing.Streaming/Controllers/StatusController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Theta.Platform.UI.Pricing.Streaming.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var assemblyVersion = assembly.GetName().Version;
+
+            return new OkObjectResult(new { version = assemblyVersion.ToString() });
+        }
+    }
+}
