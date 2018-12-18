284dc555dee000bcfac096542b09a19cfd180671
Ben Arroyo <ben.arroyo@ringleadersolutions.com>
2018-12-18 13:50:31 +0000

    Extended notification types

---
+++ src/Theta.Platform.UI.Pricing.Streaming/Domain/Notifications/NotificationType.cs
@@ -0,0 +1,10 @@
+﻿namespace Theta.Platform.UI.Pricing.Streaming.Domain.Notifications
+{
+    public enum NotificationType
+    {
+        ORDER_PICKED_UP,
+        ORDER_AMENDED,
+        ORDER_CANCELLED,
+        SYSTEM_NOTIFICATION
+    }
+}
---
+++ src/Theta.Platform.UI.Pricing.Streaming/Domain/Notifications/OrderNotification.cs
@@ -0,0 +1,15 @@
+﻿namespace Theta.Platform.UI.Pricing.Streaming.Domain.Notifications
+{
+    public class OrderNotification : Notification
+    {
+        public string OrderId { get; set; }
+
+        public string Event { get; set; }
+
+        public string Time { get; set; }
+
+        public string Operation { get; set; }
+
+        public string AuditInfo { get; set; }
+    }
+}
